29bba0ea6a96c7bf703a355464176358de693e93
GustavSkou <Gustav.skou.rasmussen@gmail.com>
2025-10-30 10:32:09 +0100

    fixes

--- Program.cs
+++ Program.cs
@@ -1,33 +1,23 @@
-using BeerMachineApi;
+using BeerMachineApi.Services;
 public class Program
 {
     static void Main(string[] args)
     {
         // Run the BeerMachineHandler on a thread so the process does not block the api
 
-        BeerMachineApi.Models.IMachineService machineService = new BeerMachineService(new BeerMachineApi.Models.BeerMachineStatusModel());
+        IMachineService machineService = new BeerMachineService(new BeerMachineStatusModel());
         Thread thread = new Thread(machineService.Start);
         thread.Start();
 
-        // build and run api
-        var builder = WebApplication.CreateBuilder(args);
-
-        // Add services to the container.
-
-        builder.Services.AddControllers();
-        // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-        builder.Services.AddOpenApi();
-
+        var builder = WebApplication.CreateBuilder(args); // build and run api
+        builder.Services.AddControllers(); // Add services to the container.
+        builder.Services.AddOpenApi(); // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
         builder.Services.AddSingleton(machineService);
-
         var app = builder.Build();
-
-        // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
+        if (app.Environment.IsDevelopment()) // Configure the HTTP request pipeline.
         {
             app.MapOpenApi();
         }
-
         app.UseHttpsRedirection();
         app.UseAuthorization();
         app.MapControllers();
