8b1e98942f22faf7e8e55e9977d9d981beec288b
GustavSkou <Gustav.skou.rasmussen@gmail.com>
2025-11-05 23:44:43 +0100

    update inventory at start

--- Services/BeerMachineService.cs
+++ Services/BeerMachineService.cs
@@ -56,6 +56,8 @@ public class BeerMachineService : MachineCommands, IMachineService
         {
             ConnectToServer(_opcClient);
 
+            _inventoryStatusModel.UpdateModel(_opcClient);
+
             OpcSubscribeDataChange[] subscriptions = {
                 new OpcSubscribeDataChange(NodeIds.AdminProcessedCount, HandleProcessedChange),
                 new OpcSubscribeDataChange(NodeIds.Barley, HandleInventoryChange),
