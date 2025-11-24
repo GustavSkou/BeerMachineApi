49d15f031231ef66b1b6afb091ed6dfd56bfd83e
GustavSkou <Gustav.skou.rasmussen@gmail.com>
2025-11-24 12:07:28 +0100

    service tests

---
+++ BeerMachineApi.Tests/ServiceTests.cs
@@ -0,0 +1,43 @@
+﻿using BeerMachineApi.Controllers;
+using BeerMachineApi.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeerMachineApi.Tests.Service;
+
+public class ServiceTests
+{
+    [SetUp]
+    public void Setup ()
+    {
+
+    }
+
+    [Category ( "test" )]
+    [Test]
+    public void ConnectToMachine ()
+    {
+
+    }
+
+    [Category ( "test" )]
+    [Test]
+    public void CommandQueue ()
+    {
+
+    }
+
+    [Category ( "test" )]
+    [Test]
+    public void ProcessCommand ()
+    {
+
+    }
+}
